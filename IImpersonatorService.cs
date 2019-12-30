b411a6bbc59658902c59c53443d14f6bfb26fff9
Ian Baker <imbaker1234@gmail.com>
2019-12-30 01:49:54 -0500

    Initial

---
+++ IImpersonatorService.cs
@@ -0,0 +1,53 @@
+﻿namespace AdminTools
+{
+    public interface IImpersonatorService
+    {
+        /// <summary>
+        /// The username of the user associated with this Service.
+        /// </summary>
+        string User { set; }
+
+        /// <summary>
+        /// The password of the user associated with this Service.
+        /// </summary>
+        string Pass { set; }
+
+        /// <summary>
+        /// The domain of the user associated with this Service. This will default to
+        /// the domain of the current user if not set.
+        /// </summary>
+        string Domain { set; }
+
+        /// <summary>
+        /// Attempt to authorize either using the provided credentials, or if none are provided, a
+        /// default to the class level credentials. If the authorization fails due to incorrect
+        /// credentials or lack of both class and argument level parameters this will return null
+        /// causing the using block's code to execute as the current user.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        Impersonator TryImpersonate(string user = null, string pass = null, string domain = null);
+
+        /// <summary>
+        /// <para>
+        /// Provides an Impersonator to allow the program to act using another users permissions.
+        /// </para>
+        /// <para>
+        /// All parameters are optional, defaulting to the class level variables if set, and
+        /// overriding the class level variables if provided.
+        /// </para>
+        /// <para>
+        /// Multiple levels of impersonation are not permitted by this service but can be achieved
+        /// by calling for the Impersonator object directly. This avoids wasteful or complex logic
+        /// and avoids re-impersonating on recursive calls.
+        /// </para>
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        Impersonator Impersonate(string user = null, string pass = null, string domain = null);
+    }
+}
---
+++ ImpersonatorService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace AdminTools
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("945B8524-2482-42E6-9E1B-68827007D541")]
+    [ProgId("Prometric.AdminTools.AdminService")]
+    [ComDefaultInterface(typeof(IImpersonatorService))]
+    [AutomationProxy(true)]
+    public class ImpersonatorService : IImpersonatorService
+    {
+        public string User { private get; set; }
+        public string Pass { private get; set; }
+        public string Domain { private get; set; }
+
+        public ImpersonatorService()
+        {
+        }
+
+        public ImpersonatorService(string user, string pass, string domain = "")
+        {
+            User = user;
+            Pass = pass;
+            Domain = domain is "" ? Environment.UserDomainName : domain;
+        }
+
+        /// <summary>
+        /// Attempt to authorize either using the provided credentials, or if none are provided, a
+        /// default to the class level credentials. If the authorization fails due to incorrect
+        /// credentials or lack of both class and argument level parameters this will return null
+        /// causing the using block's code to execute as the current user.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        public Impersonator TryImpersonate(string user = null, string pass = null, string domain = null)
+        {
+            try
+            {
+                return Impersonate(user, pass, domain);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Impersonator Impersonate(string user = null, string pass = null, string domain = null)
+        {
+            if (WindowsIdentity.GetCurrent().ImpersonationLevel != TokenImpersonationLevel.None) return null;
+            if (user is null) user = User ?? throw new ArgumentException("User is neither set at the class level nor was it provided as an argument.");
+            if (pass is null) pass = Pass ?? throw new ArgumentException("Pass is neither set at the class level nor was it provided as an argument.");
+            if (domain is null) domain = Domain ?? Environment.UserDomainName;
+
+            return new Impersonator(user, pass, domain);
+        }
+    }
+}
